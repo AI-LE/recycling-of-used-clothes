e8f18b1a3343bface972f39e00c1afca7654115a
qince <39437748@qq.com>
2019-07-18 16:13:31 +0800

    获得授权

--- src/main/java/com/mbyte/easy/recycle/controller/PubController.java
+++ src/main/java/com/mbyte/easy/recycle/controller/PubController.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.mbyte.easy.common.controller.BaseController;
 import com.mbyte.easy.common.web.AjaxResult;
 import com.mbyte.easy.recycle.entity.WeixinUser;
+import com.mbyte.easy.recycle.service.IWeixinUserService;
 import com.mbyte.easy.vo.WeChatAppLoginReq;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +31,8 @@ import java.util.Map;
 @RequestMapping("/pub")
 public class PubController extends BaseController {
 
+    @Autowired
+    private IWeixinUserService weixinUserService;
     /**
      * TODO 小程序appid
      **/
@@ -45,7 +49,7 @@ public class PubController extends BaseController {
      * 获得微信授权
      */
     @RequestMapping(value = {"/getOpenId"})
-    public AjaxResult getOpenId(@ModelAttribute WeChatAppLoginReq req, String encryptedData, String iv) throws IOException {
+    public AjaxResult getOpenId(@ModelAttribute WeChatAppLoginReq req) throws IOException {
 
         /**
          * TODO 小程序登录时获取的 code
@@ -60,18 +64,19 @@ public class PubController extends BaseController {
         CloseableHttpClient client = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(url);
         CloseableHttpResponse res = client.execute(httpGet);
+
         if (res.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             HttpEntity entity = res.getEntity();
             String result = EntityUtils.toString(entity, "UTF-8");
             JSONObject jsonObject = JSON.parseObject(result);
-            WeixinUser weixinUser = new WeixinUser();
-            System.out.println(jsonObject);
-            String openid = jsonObject.get("openid").toString();
-            weixinUser.setOpenId(openid);
-            QueryWrapper<WeixinUser> queryWrapper = new QueryWrapper<WeixinUser>();
-            if (weixinUser.getOpenId() != null && !"".equals(weixinUser.getOpenId())) {
-                queryWrapper = queryWrapper.eq("openId", weixinUser.getOpenId());
-            }
+
+//            String openid = jsonObject.get("openid").toString();
+//            WeixinUser weixinUser = new WeixinUser();
+//            weixinUser.setOpenId(openid);
+//            QueryWrapper<WeixinUser> queryWrapper = new QueryWrapper<WeixinUser>();
+//            if (weixinUser.getOpenId() != null && !"".equals(weixinUser.getOpenId())) {
+//                queryWrapper = queryWrapper.eq("openId", weixinUser.getOpenId());
+//            }
 //            WeixinUser oldUser = weixinUserService.getOne(queryWrapper);
 //            weixinUser.setCreatetime(LocalDateTime.now());
 //
@@ -80,6 +85,7 @@ public class PubController extends BaseController {
 //            } else {
 //                weixinUserService.save(weixinUser);
 //            }
+
             Map<String, Object> map = new HashMap<String, Object>();
             String openId = jsonObject.getString("openid");
             map.put("openId", openId);
@@ -88,6 +94,7 @@ public class PubController extends BaseController {
 
         return error();
     }
+
 
 
 }
